f14efb8199b90c25961c885651dd7cecbdc44e34
BURHANUDDIN <Bmerchant945@gmail.com>
2020-11-12 13:14:22 +0530

    added a collapsable nav-bar in mobile view

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
+  let showNav = false;
+  const onClickShow = () => {
+    if (showNav) {
+      document.getElementById("nav-ul").style.display = "flex";
+    } else {
+      document.getElementById("nav-ul").style.display = "none";
+    }
+    showNav = !showNav;
+  };
   return (
     <nav>
-      <ul>
+      <button id="show-nav" onClick={onClickShow}>
+        =
+      </button>
+      <ul id="nav-ul">
         <li>
           <Link to="/">Home</Link>
         </li>
