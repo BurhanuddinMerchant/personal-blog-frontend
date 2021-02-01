9045de18fd429ee5d07859d23030b934bbd6d361
BURHANUDDIN <Bmerchant945@gmail.com>
2021-02-01 22:37:37 +0530

    added a collapsable navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Title from "./Title";
 const Header = (props) => {
+  const [showNav, setShowNav] = useState(true);
+  const handleClick = (e) => {
+    console.log(e);
+    e.preventDefault();
+    if (showNav) {
+      document.getElementById("main-navbar").style["display"] = "flex";
+      document.getElementById("main-navbar").style["margin"] = "auto";
+    } else {
+      document.getElementById("main-navbar").style.display = "none";
+    }
+    setShowNav(!showNav);
+  };
   return (
     <header>
-      <Title />
+      <div className="title-button-container">
+        <Title />
+        <button id="shownav" onClick={handleClick}>
+          =
+        </button>
+      </div>
       <Navbar navActive={props.navActive} />
     </header>
   );
--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const Navbar = (props) => {
-  console.log(props.navActive);
   const { home, allblogs, about, admin } = props.navActive;
+
   return (
-    <nav>
+    <nav id="main-navbar">
       <ul id="nav-ul">
         <li>
           <Link to="/" className={home}>
