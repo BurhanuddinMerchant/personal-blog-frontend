e39502e8b5bde1d1a62a073083a6b012eb825fdc
BURHANUDDIN <Bmerchant945@gmail.com>
2021-02-02 00:08:58 +0530

    finished up about me

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import svg from "./undraw_Content_re_33px.svg";
+import svg from "./about.svg";
 import Header from "../header/Header";
 const Login = () => {
   console.log("about");
@@ -7,9 +7,36 @@ const Login = () => {
     <>
       <Header navActive={{ about: "active" }} />
       <main>
-        <div style={{ textAlign: "center" }}>
-          <h1>Welcome To About</h1>
-          <img className="home-image" alt="home" src={svg} />
+        <div className="about-container" style={{ textAlign: "center" }}>
+          <h1>About Me</h1>
+          <div className="about-content">
+            <img className="home-image" alt="home" src={svg} />
+            <p>
+              I am Burhanuddin Merchant, a Second Year Computer Engineering
+              undergraduate student at 'Pune Institute of Computer Technology'.
+              My key areas of interest are Web Development, The Cloud, DevOps
+              and Machine Learning. I love exploring technologies and building
+              projects using them. I usually work with MERN Stack for web
+              development. Thank You for checking this project out. You can
+              check out more about me by clicking the links below.
+              <div>
+                <a
+                  href="https://github.com/BurhanuddinMerchant"
+                  className="github"
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+                <a
+                  className="linkedin"
+                  href="https://www.linkedin.com/in/burhanuddin-merchant-89b14a198/"
+                  target="_blank"
+                >
+                  LinkedIN
+                </a>
+              </div>
+            </p>
+          </div>
         </div>
       </main>
     </>
